fae51ac2249d851d0bfa2da807aa580802a58dd4
front42 <altaifront@gmail.com>
2021-02-12 21:05:55 +0300

    init: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+	let brackets = bracketsConfig.join(',').split(',').join('');
+
+	let stack = [];
+
+	for (let strBracket of str) {
+		let bracketsIndex = brackets.indexOf(strBracket);
+		
+ 		if (bracketsIndex % 2 === 0) {
+			if (strBracket === brackets[bracketsIndex + 1] && stack[stack.length - 1] === bracketsIndex) {
+					stack.pop();
+			} else if (strBracket === brackets[bracketsIndex + 1] && stack[stack.length - 1] !== bracketsIndex) {
+					stack.push(bracketsIndex);
+			} else {
+					stack.push(bracketsIndex);
+      }   
+		} else {
+			if (stack.pop() !== bracketsIndex - 1) {
+				return false;
+			}
+		}
+	} return stack.length === 0;
 }
